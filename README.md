a925051ae6a43cb9e9e007a57bc458ea8eac4b13
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2021-11-20 18:19:16 -0700

    get input and learn

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Run:   
 > python markov.py <dictonary file> <input file>   
 dictionary file and input file are optional
+
+
+This is from a tutorial by 0612:
+https://www.youtube.com/watch?v=L97yQMT0jn8
--- markov.py
+++ markov.py
@@ -7,9 +7,16 @@ def main():
 
     if inputFile == "":
         # Interactive Mode
+        while True:
+            userInput = raw_input(">> ")
+            if userInput == "":
+                break
 
+            dictionary = learn(dictionary, userInput)
+            updateFile(dictionaryFile, dictionary)
     else:
         # Read from File
+        print("Not yet implemented")
 
 
 
@@ -36,3 +43,30 @@ def loadDictionary(filename):
     dictionary = json.load(file)
     file.close()
     return dictionary
+
+
+def learn(dict, input):
+    tokens = input.split(" ")
+    for i in range(0, len(tokens -1):
+        currentWord = tokens[i]
+        nextWord = tokens[i+1]
+
+        if currentWord not in dict:
+            # Create a new entry in the dictonary
+            dict[currentWord] = {nextWord: 1}
+        else:
+            allNextWords = dict[currentWord]
+
+            if nextWord not in allNextWords:
+                # Add new next state
+                dict[currentWord][nextWord] = 1
+            else:
+                dict[currentWord][nextWord] = dict[currentWord][nextWord] + 1
+
+    return dict
+
+
+def updateFile(filename, dictionary):
+    file = open(filename, "w")
+    json.dump(dictionary, file)
+    file.close()
